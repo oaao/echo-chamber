ac6772ccd0a5f8191a9b397f98227ede9634a088
Oliver Antoniu <oliver.antoniu@gmail.com>
2019-10-07 13:40:39 -0600

    add candidacy introduction

---
+++ candidacy/README.md
@@ -0,0 +1,9 @@
+# /candidacy
+
+Below, a manual selection of promising pairs and currently active ~~demons~~ bots.
+
+In this directory, the source files for creating possible combinations.
+
+## active
+
+## shortlist
---
+++ candidacy/candidates.py
@@ -0,0 +1,39 @@
+import itertools
+
+import pprint
+
+candidates = [
+
+    # category
+    'cryptocurrency ICOs',
+
+    # text
+    'Rupi Kaur poems',
+    'King James Bible',
+    'The Communist Manifesto',
+    'The Satanic Bible',
+    'Sense and Sensibility',
+    'Frankenstein',
+    'The Brothers Karamazov',
+    'SICP',
+
+    # fictional character
+    'TNG - Data',
+    'TNG - Picard',
+
+    # person group
+
+    # person
+    'Deepak Chopra',
+    'Elon Musk',
+    'Mark Zuckerberg - testimony',
+    'Richard Stallman',
+    'Linus Torvalds',
+    'Donald Trump',
+    'Greta Thunberg',
+    'Alex Jones',
+    'Barack Obama',
+
+    # resource (encyclopedia-like)
+    'Erowid',
+]
